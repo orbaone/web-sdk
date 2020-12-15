05af5e9aa1e3c7285a12be57bde7997179e58d81
Jordan Jones <proxima.aust@gmail.com>
2020-12-15 15:38:43 -0500

    fix, use browser communication channel

--- packages/core/src/elements/constants.ts
+++ packages/core/src/elements/constants.ts
@@ -1,3 +1,3 @@
 export const ORBA_ONE_SUCCESS = "orba_one_success";
 export const ORBA_ONE_CANCEL = "orba_one_cancel";
-export const ORBA_ONE_MESSAGE_CHANNEL = "orba_one_message_channel";
+export const ORBA_ONE_MESSAGE_CHANNEL = "message";
