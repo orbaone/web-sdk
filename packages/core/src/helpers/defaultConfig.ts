d1b78443e964d4f7c8885e94919588dbb846e094
Jordan Jones <proxima.aust@gmail.com>
2020-12-15 13:32:49 -0500

    feat, use prod app api url

--- packages/core/src/helpers/defaultConfig.ts
+++ packages/core/src/helpers/defaultConfig.ts
@@ -1,3 +1,3 @@
-export const apiUrl = "https://app.t3std3v.orbaone.com/api/v1";
+export const apiUrl = "https://app-api.orbaone.com/api/v1";
 export const verificationUrl = "https://verify.orbaone.com/";
 export const orbaOneLoaderId = "orba-one-loader";
